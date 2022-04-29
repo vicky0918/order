f6823b981b4a954c327fb1c6c451d6c8bf7e6320
User <vickywan90@gmail.com>
2022-04-29 16:03:20 +0800

    4/29 class

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -1,11 +1,18 @@
 package com.fju.order;
 
-public class Order {
+public class Order{
     int amount;
     Delivery delivery;
+    int total;
+    int shipFeed;
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+        this.shipFeed = delivery.price;
+    }
+    public int total() {
+        total = amount + delivery.price;
+        return total;
     }
 }
